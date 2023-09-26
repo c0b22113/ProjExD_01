78c4636c543b671b6227e8e64abd03492bda3d5b
c0b22113 <c0b2211320@edu.teu.ac.jp>
2023-09-26 15:24:11 +0900

    10°回転、リスト作成

--- pygame_image.py
+++ pygame_image.py
@@ -7,14 +7,19 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
-    ch_img = pg.transform.flip(pg.image.load("ex01/fig/3.png"), True, False)
+    kk3_img = pg.image.load("ex01/fig/3.png")
+    kk3_img = pg.transform.flip(kk3_img ,True, False)
+    kk2_img = pg.image.load("ex01/fig/2.png")
+    kk2_img_10 = pg.transform.rotate(kk2_img, 10)
+    kk2_list = [kk2_img, kk2_img_10]
+
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
         screen.blit(bg_img, [0, 0])
-        screen.blit(ch_img, [300, 200])
+        screen.blit(kk3_img, [300, 200])
         pg.display.update()
         tmr += 1
         clock.tick(10)
